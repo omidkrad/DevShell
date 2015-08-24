567e8a8fc69d9a2cc0912ae8ad652897b010412a
Omid K. Rad <omidkrad@users.noreply.github.com>
2015-08-24 11:45:31 -0700

    Update Find-String

--- scripts/Find-String.ps1
+++ scripts/Find-String.ps1
@@ -1,6 +1,33 @@
-﻿function Find-String
+﻿# Wrapper cmdlet for FINDSTR.EXE for the most common usages.
+# By default it searches strings literally (no regular expressions).
+
+function Find-String
 {
-    & findstr.exe /spin /a:A $args
+    Param(
+        [string]$SearchString,
+        [string]$Files = '*',
+        [switch]$Recurse = $true,
+        [switch]$IgnoreCase = $true,
+        [switch]$AtBeginningOfLine,
+        [switch]$AtEndOfLine,
+        [switch]$SkipBinaryFiles = $true,
+        [switch]$PrintLineNumber = $true,
+        [switch]$PrintSeekOffset,
+        [switch]$FileNamesOnly,
+        [switch]$AsRegEx
+    )
+    $switches = ''
+    if ($Recurse) { $switches += 's' }
+    if ($IgnoreCase) { $switches += 'i' }
+    if ($AtBeginningOfLine) { $switches += 'b' }
+    if ($AtEndOfLine) { $switches += 'e' }
+    if ($SkipBinaryFiles) { $switches += 'p' }
+    if ($PrintLineNumber) { $switches += 'n' }
+    if ($PrintSeekOffset) { $switches += 'o' }
+    if ($FileNamesOnly) { $switches += 'm' }
+    if ($AsRegEx) { $switches += 'r' }
+    if ($switches) { $switches = '/' + $switches }
+    & findstr.exe /a:A $switches /c:"$SearchString" $Files
 }
 Set-Alias fs Find-String
 
